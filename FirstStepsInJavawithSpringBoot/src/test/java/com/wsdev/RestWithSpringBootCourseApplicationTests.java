3f2093ba9a3a015d694b4ad269b1d9971f71ec3e
Weslei <sschmittweslei@gmail.com>
2025-11-16 19:49:54 -0300

    First commit

---
+++ FirstStepsInJavawithSpringBoot/src/test/java/com/wsdev/RestWithSpringBootCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wsdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringBootCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
